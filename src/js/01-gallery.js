bc382cdb48be7bf3ddf74fd4ae7adcc266d5985d
Kopchuk Maksym <kopchuk.maksym@gmail.com>
2022-07-11 15:31:51 +0300

    done 1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const blockGallery = document.querySelector('.gallery');
+
+const addedImg = () => {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <a class="gallery__item" href="${original}">
+    <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+    />
+    </a>`;
+    })
+    .join('');
+};
+
+const imagesBlock = addedImg();
+blockGallery.insertAdjacentHTML('afterbegin', imagesBlock);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionDelay: 250,
+});
