15427e3af18589f14afedc888836d6582363c35e
Kopchuk Maksym <kopchuk.maksym@gmail.com>
2022-07-11 18:58:15 +0300

    done 2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+let currentTime = localStorage.getItem('videoplayer-current-time');
+currentTime = currentTime === null ? 0 : currentTime;
+
+const onPlay = function ({ seconds }) {
+  console.log(seconds);
+  localStorage.setItem('videoplayer-current-time', seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(currentTime);
