b055a0619a85cdd2f8f088af94e4660913774bcd
Kopchuk Maksym <kopchuk.maksym@gmail.com>
2022-07-12 16:22:50 +0300

    done 3 task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
+import localStorageCommands from './localstorage';
 
+const formEl = document.querySelector('.feedback-form');
+const inputEl = document.querySelector('input');
+const textareaEl = document.querySelector('textarea');
+const fEEDBACK_FORM_STATE = 'feedback-form-state';
+const resultDataLS = localStorageCommands.load(fEEDBACK_FORM_STATE);
+const data = {
+  email: '',
+  message: '',
+};
+if (resultDataLS !== undefined) {
+  inputEl.value = resultDataLS.email;
+  textareaEl.value = resultDataLS.message;
+}
+
+const formFields = event => {
+  console.log('cccc');
+  const {
+    elements: { email, message },
+  } = formEl;
+  data.email = email.value;
+  data.message = message.value;
+  localStorageCommands.save(fEEDBACK_FORM_STATE, data);
+};
+
+const removeDataLS = event => {
+  event.preventDefault();
+  localStorageCommands.remove(fEEDBACK_FORM_STATE);
+  console.log(data);
+  event.currentTarget.reset();
+};
+
+formEl.addEventListener('submit', removeDataLS);
+
+formEl.addEventListener('input', throttle(formFields, 500));
